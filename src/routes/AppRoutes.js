7068714f1dd9c7cc7b045a2d5e652df2b19f12b9
willy <willydasilva29@gmail.com>
2023-03-02 21:29:59 -0300

    iniciando navbar e footer

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
+import Navbar from '../components/Navbar/Navbar';
 import About from '../pages/About/About';
 import Home from '../pages/Home/Home';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
