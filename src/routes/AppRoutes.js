f52bad5a9b83047c05c5389bf72610e5e54af1ad
willy <willydasilva29@gmail.com>
2023-03-11 22:30:30 -0300

    context da autenticação

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -7,23 +7,26 @@ import Home from '../pages/Home/Home';
 import "../App.css";
 import Login from '../pages/Login/Login';
 import Registro from '../pages/Registro/Registro';
+import { AuthProvider } from '../context/AuthContext';
 
 function AppRoutes() {
   return (
     <div className='App'>
 
-      <BrowserRouter>
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/registro' element={<Registro />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/registro' element={<Registro />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
 
 
     </div>
