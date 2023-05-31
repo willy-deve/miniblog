17dd7d78850a885716699ba0c467d1ef5912fc4a
willy <willydasilva29@gmail.com>
2023-05-31 19:25:47 -0300

    estruturando a home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Home.module.css"
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return (
-    <React.Fragment>
-      <h1>Home</h1>
 
-    </React.Fragment>
+  const [query, setQuery] = useState("")
+  const [posts] = useState([])
+
+  return (
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form className={styles.search_form}>
+        <input
+          type='text'
+          placeholder='ou busque por tags.'
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className='btn btn-dark'>Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram enconstrados posts</p>
+            <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
